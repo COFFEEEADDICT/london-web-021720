f547d67ce2c2d69fa44f07cec539e1e1b2583635
Sam Barker <samuel.e.barker@googlemail.com>
2020-03-30 11:31:00 +0100

    18 dom

---
+++ 18-js-dom-manipulation/README.md
@@ -0,0 +1,16 @@
+Real time messages
+
+Like buttons
+
+One page, multiple uses
+
+Dynamic forms
+
+Canvas (games)
+
+DOM Manipulation
+CRUD DOM elements
+
+Events
+
+API requests
---
+++ 18-js-dom-manipulation/index.js
@@ -0,0 +1,66 @@
+// r
+
+document.querySelector("");
+
+// tag
+
+document.querySelector("div");
+document.querySelector("a");
+document.querySelector("p");
+document.querySelector("h1");
+
+// id
+
+document.querySelector("#u19184803273642448");
+document.querySelector("#u19184803273642448").querySelector("a");
+
+// class
+
+document.querySelector(".gs-c-promo-heading");
+document.querySelectorAll(".gs-c-promo-heading");
+document.querySelector(".gs-c-promo-heading.gel-pica-bold");
+
+// CSS selectors
+document.querySelector(".gs-c-promo-heading:last-child");
+document.querySelector("#u19184803273642448 a");
+document.querySelector(".gel-layout.gel-layout--equal img");
+
+// u
+const image = document.querySelector(".gel-layout.gel-layout--equal img");
+
+image.src = "image.jpg";
+
+const headline = {};
+
+headline.innerText = "Don't panic";
+
+const button = document.querySelector("button");
+
+button.disabled = true;
+
+// c
+
+// create element
+const div = document.createElement("div");
+// update element
+div.innerText = "Corona no more";
+// display element
+
+const parent = document.querySelector("a");
+parent.append(div);
+
+//create
+
+// js how to create a div
+
+//update
+
+// js how to display text inside div
+
+//display
+
+// js how to display element on page
+
+// d
+
+element.remove();
